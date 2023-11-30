91dab68451214997476700353e6da0c07e7fa737
Pyae Phyo Zan <pyae418283@gmail.com>
2023-11-30 12:23:21 +0630

    add fillable,relation and soft delete

--- app/Models/RoomCategory.php
+++ app/Models/RoomCategory.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RoomCategory extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        "room_type",
+        "room_img",
+        "capacity",
+        "description",
+        "cost",
+    ];
+
+    public function rooms(){
+        return $this->hasMany(Room::class);
+    }
+
 }
--- app/Models/Transcation.php
+++ app/Models/Transcation.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transcation extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        "transcation_name",
+        "booking_id",
+    ];
+
+
 }
