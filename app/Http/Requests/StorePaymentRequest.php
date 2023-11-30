7868f3e85ebedd612a48f3cd309131c6a05518d0
Pyae Phyo Zan <pyae418283@gmail.com>
2023-11-30 12:27:52 +0630

    add new requests for room,room category,booking,payment

---
+++ app/Http/Requests/StorePaymentRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "card_no" => [
+                "required"
+            ],
+            "exp_month" => [
+                "required"
+            ],
+            "exp_year" => [
+                "required"
+            ],
+            "cvv" => [
+                "required"
+            ],
+        ];
+    }
+}
---
+++ app/Http/Requests/StoreRoomRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Gate;
+
+class StoreRoomRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Gate::allows("room_create");
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "room_category_id" => [
+                "required"
+            ],
+            "room_no" => [
+                "required",
+                "string"
+            ]
+        ];
+    }
+}
