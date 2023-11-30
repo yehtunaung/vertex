606d41e2e3dd4841fe52ef4f3efdd06d1c602ac9
Pyae Phyo Zan <pyae418283@gmail.com>
2023-11-30 12:20:35 +0630

    add auto run script

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,11 @@
 
 namespace App\Console;
 
+use App\Models\RoomCategory;
+use App\Models\Room;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +28,31 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        // clean trash data after 30 day restore
+        $schedule->call(function(){
+
+            $day = now()->subDays(30)->toDateTimeString();
+            // clean room trash
+            $rooms = Room::whereNotNull("deleted_at")
+            ->where("deleted_at", "<=", $day)
+            ->get();
+
+            $rooms->each->forceDelete();
+
+            // clean room category
+            $roomCategories = RoomCategory::whereNotNull("deleted_at")
+            ->where("delete_at","<=",$day)
+            ->get();
+
+            foreach($roomCategories as $roomCategory){
+                if(Storage::exists("public/rooms/".$roomCategory->room_img)){
+                    Storage::delete("public/rooms/".$roomCategory->room_img);
+                }
+
+                $roomCategory->forceDelete();
+            }
+        });
     }
 
     /**
